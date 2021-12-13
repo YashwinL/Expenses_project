daa7e25564783e5a407414591f496003efe04ab2
YashwinL <yashwinlakshmipathy@gmail.com>
2021-12-14 01:34:32 +0530

    commit

--- src/components/NewExpenses/ExpensesForm.js
+++ src/components/NewExpenses/ExpensesForm.js
@@ -1,20 +1,59 @@
 import "./ExpensesForm.css";
+import { useState } from "react";
 
 const ExpensesForm = () => {
+  const [Enteredtitle, setTitle] = useState("");
+  const [Enteredamount, setAmount] = useState("");
+  const [Entereddate, setDate] = useState("");
+  const titleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const amountHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const dateHandler = (event) => {
+    setDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: Enteredtitle,
+      amount: Enteredamount,
+      date: new Date(Entereddate),
+    };
+    console.log(expenseData);
+    setTitle("");
+    setAmount("");
+    setDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" value={Enteredtitle} onChange={titleHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={Enteredamount}
+            onChange={amountHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31 "
+            value={Entereddate}
+            onChange={dateHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
